d0d6780f683119ad418f0b2b28bb95a8a234d92f
Edwell22 <edwellkakunguwo358@gmail.com>
2024-03-29 15:46:47 +0200

    new

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,29 +3,31 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product){
     return `
     <li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
+    <a href="/product_pages/index.html?product=${product.Id}">
     <img
-      src="${product.Image}"
+      src="${product.Images.PrimaryMedium}"
       alt="${product.Name}"
     />
     <h3 class="card__brand">${product.Brand.Name}</h3>
-    <h2 class="card__name">${product.Name}</h2>
+    <h2 class="card__name">${product.NameWithoutBrand}</h2>
     <p class="product-card__price">${product.FinalPrice}</p></a>
   </li>
     `
 }
 
-
 export default class ProductList{
-    constructor(category, dataSource, listElement) {
+    constructor(category, dataSource, listElement, sort) {
       this.category = category;
+      this.sort = sort;
       this.dataSource = dataSource;
       this.listElement = listElement;
     }
     async init() {
-        const productList = await this.dataSource.getData();
-        let filterList = this.filterProductList(productList)
-        this.renderList(filterList);
+        const list = await this.dataSource.getData(this.category, this.sort);
+        //let filterList = this.filterProductList(list)
+        //this.renderList(filterList);
+        this.renderList(list);
+        document.querySelector('.title').innerHTML = this.category;
     }
     renderList(productList){
         renderListWithTemplate(productCardTemplate,this.listElement,productList);
@@ -36,4 +38,5 @@ export default class ProductList{
             return ['880RR', '985RF', '985PR', '344YJ'].includes(item.Id);
         });
     }
-  }
+    
+}
